d84756d57d90910d633d027fce6dbcba57c55478
domfiorino <dfiorino@nd.edu>
2022-11-07 13:11:11 -0500

    Commit auth modules and protected routes

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -2,8 +2,13 @@ import React from "react";
 import HomeModule from "./Home/Home.js";
 import DuncanModule from "./Duncan/Duncan.js";
 import NavigationModule from "./Navigation/Navigation";
+
+import AuthModule from "./Auth/Auth.js";
+import AuthRegister from "./Auth/AuthRegister";
+import AuthLogin from "./Auth/AuthLogin";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute.js";
 // import BrowserRouter as alias
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Routes, Route } from "react-router-dom";
 
 const Components = () => {
   return (
@@ -11,18 +16,30 @@ const Components = () => {
       <NavigationModule />
       <Routes>
         {/* Home Page - page 1 */}
-        <Route path="/" element={<HomeModule />} />
+        {/* <Route path="/" element={<HomeModule />} /> */}
+
+
         {/* Duncan menu page - page 2 */}
-        <Route path="/Duncan" element={<DuncanModule />} />
+        <Route 
+          path="/Duncan" 
+          element={<ProtectedRoute path="/" element={DuncanModule} />}
+        />
+        {/* Auth page */}
+        <Route path="/auth" element={<AuthModule />} />
+        {/* Auth/register page */}
+        <Route path="/auth/register" element={<AuthRegister />} />
+        {/* Auth/login page */}
+        <Route path="/auth/login" element={<AuthLogin />} />
+        {/* Can only access Home page via login; is protected */}
+        <Route
+          path="/"
+          element={<ProtectedRoute path="/" element={HomeModule} />}
+        />
+        {/* Redirect */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
-  // return (
-  //   <div>
-  //     <HomeModule />
-  //     <DuncanModule />
-  //   </div>
-  // );
 };
 
 export default Components;
